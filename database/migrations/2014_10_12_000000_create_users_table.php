b6e2e4a571b69ad73bbf1285b9d9b71965f75ac1
Kautsar Ibrahim Hilmi <mrhilmoy@gmail.com>
2019-05-11 21:36:27 +0700

    Seed database udah ada pake factories dan login udah tested

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,10 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
             $table->string('address');
             $table->string('phone');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
